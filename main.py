c0abd0cb2a8f9a14df234c623c6d253f7d821ca7
Gustavo Ferreira <gustavohenriquelimaa@hotmail.com>
2026-05-27 15:28:22 -0300

    Update main.py

--- main.py
+++ main.py
@@ -11,5 +11,6 @@ while True:
     for event in pygame.event.get():
         #If the event is quit, close the game
         if event.type == pygame.QUIT:
+            print('Quitting...')
             pygame.quit()# Close Window
             quit() #end pygame
